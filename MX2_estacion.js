39c118efc8a0f765d2df8d999586005c3cdd1d96
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-07-02 22:42:29 -0600

    Add files via upload

---
+++ MX2_estacion.js
@@ -0,0 +1,8 @@
+var tulti = {
+"type": "FeatureCollection",
+"name": "MX2_estacion",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Direccion": "54900. Fuentes del valle ", "Latitude": 19.641, "Longitude": -99.14, "estacion": "MX2", "station_code": "MX2", "station_name": "Tultitlan", "direccion": "54900. Fuentes del valle " }, "geometry": { "type": "Point", "coordinates": [ -99.142262198750956, 19.640306804333537 ] } }
+]
+}
